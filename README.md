b6ce80b7845b903bf5b1cf53f881bc97777c99f7
unitene <unitene@yandex.ru>
2023-10-10 14:25:35 +0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Algebra Periphery
+
+This directory contains the periphery smart contracts for the Algebra DEX. For the lower level core contracts, see the `core` directory.
+
+## Slither
+
+*Works correctly starting from the version 0.9.0*
+
+## License
+
+Smart contracts of the periphery are based on [UniswapV3 periphery](https://github.com/Uniswap/v3-periphery) and retain the GPL-2.0 or later license.
+
+Contracts have been changed in some places to be compatible with the Algebra Core and use its unique functionality.
---
+++ hardhat.config.ts
@@ -0,0 +1,79 @@
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
+import 'hardhat-watcher';
+import 'hardhat-dependency-compiler';
+import 'hardhat-contract-sizer';
+import 'solidity-coverage';
+import 'solidity-docgen';
+import baseConfig from '../../hardhat.base.config';
+
+const LOW_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.8.17',
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 2_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.8.17',
+  settings: {
+    viaIR: true,
+    optimizer: {
+      enabled: true,
+      runs: 1_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const DEFAULT_COMPILER_SETTINGS = {
+  version: '0.8.17',
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+export default {
+  networks: baseConfig.networks,
+  etherscan: baseConfig.etherscan,
+  solidity: {
+    compilers: [DEFAULT_COMPILER_SETTINGS],
+    overrides: {
+      'contracts/NonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/LimitOrderManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/MockTimeNonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/NFTDescriptorTest.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/libraries/NFTSVG.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+    },
+  },
+  typechain: {
+    outDir: 'typechain'
+  },
+  docgen: {
+    outputDir: '../../docs/Contracts/Periphery',
+    pages: (x: any) => x.name.toString() + '.md',
+    templates: '../../docs/doc_templates/public',
+    collapseNewlines: true
+  },
+  dependencyCompiler: {
+    paths: [
+      '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol',
+    ],
+  }
+}
